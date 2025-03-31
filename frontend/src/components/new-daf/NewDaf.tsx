9d7f13f06e3d9af9edee9754264632726cc23122
Stella <stellachen273@gmail.com>
2025-04-01 01:09:42 +0800

    import usestate from react as well

--- frontend/src/components/new-daf/NewDaf.tsx
+++ frontend/src/components/new-daf/NewDaf.tsx
@@ -1,4 +1,4 @@
-import type { FormEvent } from 'react';
+import { useState, FormEvent } from 'react';
 import './NewDaf.css';
 import { useMutation } from '@tanstack/react-query';
 import { getEnvOrThrow } from '../../utils/env';
@@ -60,121 +60,125 @@ export const NewDaf = () => {
   };
 
   return (
-    <form id="create-daf-form" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Fund Name</label>
-        <input type="text" id="name" name="name" required />
-      </div>
+    <div>
+      <form id="create-daf-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Fund Name</label>
+          <input type="text" id="name" name="name" required />
+        </div>
 
-      <div>
-        <label htmlFor="description">Fund Description</label>
-        <textarea id="description" name="description" required></textarea>
-      </div>
+        <div>
+          <label htmlFor="description">Fund Description</label>
+          <textarea id="description" name="description" required></textarea>
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.firstName">Advisor First Name</label>
-        <input
-          type="text"
-          id="fundAdvisor.firstName"
-          name="fundAdvisor.firstName"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.firstName">Advisor First Name</label>
+          <input
+            type="text"
+            id="fundAdvisor.firstName"
+            name="fundAdvisor.firstName"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.lastName">Advisor Last Name</label>
-        <input
-          type="text"
-          id="fundAdvisor.lastName"
-          name="fundAdvisor.lastName"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.lastName">Advisor Last Name</label>
+          <input
+            type="text"
+            id="fundAdvisor.lastName"
+            name="fundAdvisor.lastName"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.email">Advisor Email</label>
-        <input
-          type="email"
-          id="fundAdvisor.email"
-          name="fundAdvisor.email"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.email">Advisor Email</label>
+          <input
+            type="email"
+            id="fundAdvisor.email"
+            name="fundAdvisor.email"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.address.line1">
-          Advisor Address Line 1
-        </label>
-        <input
-          type="text"
-          id="fundAdvisor.address.line1"
-          name="fundAdvisor.address.line1"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.address.line1">
+            Advisor Address Line 1
+          </label>
+          <input
+            type="text"
+            id="fundAdvisor.address.line1"
+            name="fundAdvisor.address.line1"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.address.line2">
-          Advisor Address Line 2
-        </label>
-        <input
-          type="text"
-          id="fundAdvisor.address.line2"
-          name="fundAdvisor.address.line2"
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.address.line2">
+            Advisor Address Line 2
+          </label>
+          <input
+            type="text"
+            id="fundAdvisor.address.line2"
+            name="fundAdvisor.address.line2"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.address.city">Advisor City</label>
-        <input
-          type="text"
-          id="fundAdvisor.address.city"
-          name="fundAdvisor.address.city"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.address.city">Advisor City</label>
+          <input
+            type="text"
+            id="fundAdvisor.address.city"
+            name="fundAdvisor.address.city"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.address.state">Advisor State</label>
-        <input
-          type="text"
-          id="fundAdvisor.address.state"
-          name="fundAdvisor.address.state"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.address.state">Advisor State</label>
+          <input
+            type="text"
+            id="fundAdvisor.address.state"
+            name="fundAdvisor.address.state"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.address.zip">Advisor Zip</label>
-        <input
-          type="text"
-          id="fundAdvisor.address.zip"
-          name="fundAdvisor.address.zip"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.address.zip">Advisor Zip</label>
+          <input
+            type="text"
+            id="fundAdvisor.address.zip"
+            name="fundAdvisor.address.zip"
+            required
+          />
+        </div>
 
-      <div>
-        <label htmlFor="fundAdvisor.address.country">Advisor Country</label>
-        <input
-          type="text"
-          id="fundAdvisor.address.country"
-          name="fundAdvisor.address.country"
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="fundAdvisor.address.country">Advisor Country</label>
+          <input
+            type="text"
+            id="fundAdvisor.address.country"
+            name="fundAdvisor.address.country"
+            required
+          />
+        </div>
 
-      {isIdle || isError ? (
-        <button type="submit">
-          {isIdle && 'Create DAF'}
-          {isError && 'Error Creating DAF, Try Again'}
-        </button>
-      ) : (
-        <span>
-          {isPending && 'Creating DAF...'}
-          {isSuccess && 'DAF Created!'}
-        </span>
-      )}
-    </form>
+        {isIdle || isError ? (
+          <button type="submit">
+            {isIdle && 'Create DAF'}
+            {isError && 'Error Creating DAF, Try Again'}
+          </button>
+        ) : (
+          <span>
+            {isPending && 'Creating DAF...'}
+            {isSuccess && 'DAF Created!'}
+          </span>
+        )}
+      </form>
+
+      
+  </div>
   );
 };
